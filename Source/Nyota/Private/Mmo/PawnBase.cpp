36e458f68663243993250f07f21898613b522535
q847861249 <108392340+q847861249@users.noreply.github.com>
2024-05-11 20:27:58 +0930

    mmo character

--- Source/Nyota/Private/Mmo/PawnBase.cpp
+++ Source/Nyota/Private/Mmo/PawnBase.cpp
@@ -8,13 +8,38 @@ APawnBase::APawnBase()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// 修改：实例化ASC
+	AbilitySystem = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
+}
 
+UAbilitySystemComponent* APawnBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystem;
 }
 
 // Called when the game starts or when spawned
 void APawnBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (nullptr != AbilitySystem)
+	{
+		// 修改：给ASC赋予技能
+		if (HasAuthority() && MyAbilities.Num() > 0)
+		{
+			for (auto i = 0; i < MyAbilities.Num(); i++)
+			{
+				if (MyAbilities[i] == nullptr)
+				{
+					continue;
+				}
+				AbilitySystem->GiveAbility(FGameplayAbilitySpec(MyAbilities[i].GetDefaultObject(), 1, 0));
+			}
+		}
+
+		// 修改：初始化ASC
+		AbilitySystem->InitAbilityActorInfo(this, this);
+	}
 	
 }
 
--- Source/Nyota/Public/Mmo/PawnBase.h
+++ Source/Nyota/Public/Mmo/PawnBase.h
@@ -4,16 +4,34 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+
+// 修改：添加引用
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+
 #include "PawnBase.generated.h"
 
 UCLASS()
-class NYOTA_API APawnBase : public APawn
+class NYOTA_API APawnBase : public APawn,
+	public IAbilitySystemInterface //修改：继承接口
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
+
+public:
+	// 修改：申明ASC
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = GameplayAbilities, meta = (AllowPrivateAccess = "true"))
+	class UAbilitySystemComponent* AbilitySystem;
+
+	// 修改：实现接口方法
+	UAbilitySystemComponent* GetAbilitySystemComponent()const override;
+
+	// 修改：声明Ability数组
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Abilities)
+	TArray<TSubclassOf<UGameplayAbility>> MyAbilities;
 
 protected:
 	// Called when the game starts or when spawned
