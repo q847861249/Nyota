016ee19136ca5ddaf7e124fa85820846dabc9a57
q847861249 <108392340+q847861249@users.noreply.github.com>
2024-05-11 14:25:30 +0930

    Initial commit

---
+++ Source/Nyota/Nyota.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Nyota.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Nyota, "Nyota" );
+ 
