c10d1d0f7236f2c782d3e7a007abaaf8aba2337f
q847861249 <108392340+q847861249@users.noreply.github.com>
2024-05-11 19:50:33 +0930

    PawnBase

--- Source/Nyota/Private/Mmo/PawnBase.cpp
+++ Source/Nyota/Private/Mmo/PawnBase.cpp
@@ -1,21 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PawnBase.h"
-
-UAbilitySystemComponent* APawnBase::GetAbilitySystemComponent() const
-{
-	return AbilitySystem;
-}
+#include "Mmo/PawnBase.h"
 
 // Sets default values
 APawnBase::APawnBase()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	// 修改：实例化ASC
-	AbilitySystem = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
 
 }
 
@@ -24,24 +16,6 @@ void APawnBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	if (nullptr != AbilitySystem)
-	{
-		// 修改：给ASC赋予技能
-		if (HasAuthority() && MyAbilities.Num() > 0)
-		{
-			for (auto i = 0; i < MyAbilities.Num(); i++)
-			{
-				if (MyAbilities[i] == nullptr)
-				{
-					continue;
-				}
-				AbilitySystem->GiveAbility(FGameplayAbilitySpec(MyAbilities[i].GetDefaultObject(), 1, 0));
-			}
-		}
-
-		// 修改：初始化ASC
-		AbilitySystem->InitAbilityActorInfo(this, this);
-	}
 }
 
 // Called every frame
