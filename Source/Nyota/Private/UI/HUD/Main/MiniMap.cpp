b23cc91746afbeeb1af2eb4ebdc21d91a1448870
bloshen <bloshen_wangyi@163.com>
2026-05-21 00:13:22 +1200

    修复: 小地图指针和坐标映射，但目前没有解决螃蟹视角脱离后的小地图对齐问题

--- Source/Nyota/Private/UI/HUD/Main/MiniMap.cpp
+++ Source/Nyota/Private/UI/HUD/Main/MiniMap.cpp
@@ -29,7 +29,20 @@ void UMiniMap::NativeTick(const FGeometry &MyGeometry, float InDeltaTime)
     MinimalMapImage->SetRenderTranslation(NewLocation);
     //Set the pointer rotation depend on player rotation
     FRotator PawnRotate = CachedPlayerPawn->GetActorRotation();
-    PointerImage->SetRenderTransformAngle(PawnRotate.Yaw);
+    PointerImage->SetRenderTransformAngle(PawnRotate.Yaw-90.f);
+
+    // APlayerCameraManager* CameraManager = GetOwningPlayer()->PlayerCameraManager;
+    // if (!CameraManager) return;
+
+    // float CameraYaw = CameraManager->GetCameraRotation().Yaw;
+
+    // // 旋转整张小地图背景
+    // MinimalMapImage->SetRenderTransformAngle(-CameraYaw);
+
+    // // MinimalMapImage->SetRenderTransformAngle(-CameraYaw + 90.0f);
+
+    // // 箭头通常保持固定朝上
+    // PointerImage->SetRenderTransformAngle(0.0f);
 
     // APlayerCameraManager* CameraManager = GetOwningPlayer()->PlayerCameraManager;
     // if (!CameraManager) return;
