99685517ccbaf66928f403e55ba88a4818666712
John Zhao <xjzhao0326@gmail.com>
2026-08-01 21:28:12 +0800

    功能: 添加独立轻放技能

--- Source/Nyota/Private/Tests/AbilitySystem/GA_PutDownTests.cpp
+++ Source/Nyota/Private/Tests/AbilitySystem/GA_PutDownTests.cpp
@@ -2,6 +2,7 @@
 
 #include "Misc/AutomationTest.h"
 
+#include "AbilitySystem/Abilities/Player/GA_PutDown.h"
 #include "GameplayTags/GameTags.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(
@@ -26,6 +27,29 @@ bool FPutDownNativeTagsTest::RunTest(const FString &Parameters)
         TEXT("Put-down completion event"),
         Nyota::Event_Ability_PutDownEnd.GetTag().ToString(),
         FString(TEXT("Nyota.Event.Ability.PutDownEnd"))
+    );
+
+    return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+    FPutDownAbilityContractTest,
+    "Nyota.AbilitySystem.PutDown.AbilityContract",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+)
+
+bool FPutDownAbilityContractTest::RunTest(const FString &Parameters)
+{
+    const UGA_PutDown *AbilityCDO = GetDefault<UGA_PutDown>();
+
+    TestNotNull(TEXT("Put-down ability CDO"), AbilityCDO);
+    TestTrue(
+        TEXT("Ability identifies itself by exact tag"),
+        AbilityCDO->AbilityTags.HasTagExact(Nyota::Ability_Grab_PutDown)
+    );
+    TestTrue(
+        TEXT("Ability requires the put-down window"),
+        AbilityCDO->ActivationRequiredTags.HasTagExact(Nyota::Ability_State_Grabbing_PutDownWindow)
     );
 
     return true;
