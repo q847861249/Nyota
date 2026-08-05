871fa2b0a90dc56e7b2cb6292a0b7bec24389387
bloshen <bloshen_wangyi@163.com>
2026-08-05 21:58:47 +1200

    UI:已经实现资源点按状态变色，同时小地图UI可以正确的映射资源点对应状态的颜色，即将刷新状态会有倒计时显示。

--- Source/Nyota/Private/Actor/LootPoint.cpp
+++ Source/Nyota/Private/Actor/LootPoint.cpp
@@ -19,13 +19,38 @@ ALootPoint::ALootPoint()
 void ALootPoint::BeginPlay()
 {
 	Super::BeginPlay();
-    Init();	
 }
 
 // Called every frame
 void ALootPoint::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if(!IsInit) return;
+
+	FLinearColor CurrentColor = FLinearColor::Gray;
+	switch (CurrentState)
+	{
+	case ELootPointState::CoolDown:
+		CurrentColor = CoolDownColor;
+		break;
+
+	case ELootPointState::UpComing:
+		CurrentColor = UpComingColor;
+		break;
+
+	case ELootPointState::Active:
+		CurrentColor = ActiveColor;
+		break;
+
+	case ELootPointState::Remaining:
+		CurrentColor = RemainingColor;
+		break;
+
+	case ELootPointState::Completed:
+		CurrentColor = CompletedColor;
+		break;
+	}
 
 	if (bDrawDebugRange)
 	{
@@ -34,7 +59,7 @@ void ALootPoint::Tick(float DeltaTime)
 			GetActorLocation(),
 			Range,
 			64,
-			FColor::Green,
+			CurrentColor.ToFColor(true),
 			false,
 			0.0f,
 			0,
@@ -46,7 +71,46 @@ void ALootPoint::Tick(float DeltaTime)
 	}
 }
 
-void ALootPoint::Init()
+void ALootPoint::Init(float OutTime)
+{
+	IsInit = true;
+	PrepareTime = OutTime;
+	UE_LOG(LogTemp,Warning,TEXT("Prepare the LootPoint"));
+	GetWorld()->GetTimerManager().SetTimer(PrepareTimerHandle,this,&ALootPoint::SetCoolDownState,PrepareTime,false);
+}
+void ALootPoint::SetCoolDownState()
+{
+	CurrentState = ELootPointState::CoolDown;
+	UE_LOG(LogTemp,Warning,TEXT("Now,the state is CoolDown"));
+	GetWorld()->GetTimerManager().SetTimer(CoolDownTimerHandle,this,&ALootPoint::SetUpComingState,CoolDownTime,false);
+}
+void ALootPoint::SetUpComingState()
+{
+	CurrentState = ELootPointState::UpComing;
+	UE_LOG(LogTemp,Warning,TEXT("Now,the state is UpComing"));
+	GetWorld()->GetTimerManager().SetTimer(UpComingTimerHandle,this,&ALootPoint::SetActiveState,UpComingTime,false);
+}
+void ALootPoint::SetActiveState()
+{
+	CurrentState = ELootPointState::Active;
+	GenerateLoot();
+	UE_LOG(LogTemp,Warning,TEXT("Now,the state is Active"));
+	GetWorld()->GetTimerManager().SetTimer(ActiveTimerHandle,this,&ALootPoint::SetRemainingState,5.f,false);
+}
+void ALootPoint::SetRemainingState()
+{
+	CurrentState = ELootPointState::Remaining;
+	UE_LOG(LogTemp,Warning,TEXT("Now,the state is Remaining"));
+	GetWorld()->GetTimerManager().SetTimer(RemainingTimerHandle,this,&ALootPoint::SetCompleteState,5.f,false);
+}
+void ALootPoint::SetCompleteState()
+{
+	CurrentState = ELootPointState::Completed;
+	UE_LOG(LogTemp,Warning,TEXT("Now,the state is Complete"));
+	GetWorld()->GetTimerManager().SetTimer(CompletedTimerHandle,this,&ALootPoint::SetCoolDownState,5.f,false);
+}
+
+void ALootPoint::GenerateLoot()
 {
 	// caculate Loot number
     int32 LootNum = 0;
@@ -71,7 +135,6 @@ void ALootPoint::Init()
         float ValueThreshold = 0.0f;
         float RandomValue = FMath::FRandRange(0.0f, 100.0f) * Magnification;
         // float RandomValue = FMath::RandRange(0, LootArray->LootItem.Num() - 1);
-
         for(FLootValueRate& Entry:LootConfig->ValueTable)
         {
             ValueThreshold += Entry.Percent;
@@ -81,103 +144,31 @@ void ALootPoint::Init()
                 break;
             }
         }
-		UE_LOG(LogTemp,Warning,TEXT("now the LootValue is %d"),LootValue);
-        
         FLootDistributed LootArray = (LootConfig->LootTable)[LootValue];
-        // int32 RandomIndex = FMath::FRandRange(0.0f,((LootArray.LootItem).Num()-1.0f));
         int32 RandomIndex = FMath::RandRange(0, (LootArray.LootItem).Num() - 1);
         ULootDataAsset* NewDataAsset = (LootArray.LootItem)[RandomIndex];
         SpawnLootItem(NewDataAsset);
     }
 }
-
 void ALootPoint::SpawnLootItem(ULootDataAsset* DataAsset)
 {
-	if (!DataAsset || !DataAsset->LootActorClass)
-	{
-		return;
-	}
-
+	if (!DataAsset || !DataAsset->LootActorClass) return;
 	UWorld* World = GetWorld();
-	if (!World)
-	{
-		return;
-	}
 
 	UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(World);
-	if (!NavSystem)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("NavSystem is null"));
-		return;
-	}
+	if (!NavSystem) return;
 
 	const FVector LootPointLocation = GetActorLocation();
-
 	FNavLocation ProjectedCenter;
-
-	const bool bProjected = NavSystem->ProjectPointToNavigation(
-		LootPointLocation,
-		ProjectedCenter,
-		FVector(Range, Range, 5000.0f)
-	);
-
-	if (!bProjected)
-	{
-		UE_LOG(
-			LogTemp,
-			Warning,
-			TEXT("LootPoint cannot find nearby NavMesh. LootPointLocation: %s, Range: %f"),
-			*LootPointLocation.ToString(),
-			Range
-		);
-		return;
-	}
-
+	//把一个世界坐标投影到一个最近的可导航点上
+	const bool bProjected = NavSystem->ProjectPointToNavigation(LootPointLocation, ProjectedCenter, FVector(Range, Range, 5000.0f));
+	if(!bProjected ) return;
+	//找到投影点附近一个随机的可导航点
 	FNavLocation RandomNavLocation;
+	const bool bFoundNavPoint = NavSystem->GetRandomPointInNavigableRadius( ProjectedCenter.Location,Range, RandomNavLocation );
+	if (!bFoundNavPoint) return;
 
-	const bool bFoundNavPoint = NavSystem->GetRandomPointInNavigableRadius(
-		ProjectedCenter.Location,
-		Range,
-		RandomNavLocation
-	);
-
-	if (!bFoundNavPoint)
-	{
-		UE_LOG(
-			LogTemp,
-			Warning,
-			TEXT("No navigable point found. ProjectedCenter: %s, Range: %f"),
-			*ProjectedCenter.Location.ToString(),
-			Range
-		);
-		return;
-	}
-
-	const FVector TraceStart = RandomNavLocation.Location + FVector(0.0f, 0.0f, 1000.0f);
-	const FVector TraceEnd = RandomNavLocation.Location - FVector(0.0f, 0.0f, 5000.0f);
-
-	FHitResult Hit;
-	FCollisionQueryParams Params;
-	Params.AddIgnoredActor(this);
-
-	const bool bHitGround = World->LineTraceSingleByChannel(
-		Hit,
-		TraceStart,
-		TraceEnd,
-		ECC_WorldStatic,
-		Params
-	);
-
-	if (!bHitGround)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Nav point found, but ground trace failed. NavLocation: %s"),
-			*RandomNavLocation.Location.ToString()
-		);
-		return;
-	}
-
-	const FVector SpawnLocation = Hit.ImpactPoint + FVector(0.0f, 0.0f, 5.0f);
-
+	const FVector SpawnLocation = RandomNavLocation.Location + FVector(0.0f, 0.0f, 5.0f);
 	FRotator SpawnRotation = FRotator::ZeroRotator;
 	SpawnRotation.Yaw = FMath::FRandRange(0.0f, 360.0f);
 
@@ -186,18 +177,17 @@ void ALootPoint::SpawnLootItem(ULootDataAsset* DataAsset)
 	SpawnParams.SpawnCollisionHandlingOverride =
 		ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 
-	AActor* SpawnedActor = World->SpawnActor<AActor>(
-		DataAsset->LootActorClass,
-		SpawnLocation,
-		SpawnRotation,
-		SpawnParams
-	);
+	AActor* SpawnedActor = World->SpawnActor<AActor>(DataAsset->LootActorClass,SpawnLocation,SpawnRotation,SpawnParams);
 
 	if (!SpawnedActor)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Spawn Loot Actor failed"));
 	}
 
-
+}
+int32 ALootPoint::GetUpComingRemainingTime()
+{
+	if(CurrentState != ELootPointState::UpComing) return 0;
+	return FMath::CeilToInt(GetWorld()->GetTimerManager().GetTimerRemaining(UpComingTimerHandle));
 }
 
